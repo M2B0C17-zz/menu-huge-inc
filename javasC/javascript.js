547970293f63884e28e43d86d072a7c2d34a3224
Macarena Baltra <macarena.bc2011@gmail.com>
2017-05-11 14:01:38 -0300

    avance para enviar y descargar en casa

--- javasC/javascript.js
+++ javasC/javascript.js
@@ -1,17 +1,22 @@
-	document.getElementById('toggle').addEventListener('click', function(event){
+	document.getElementById("toggle").addEventListener("click", function(event){
 	event.preventDefault();
-	document.getElementById('nav-header').classList.toggle('open');
-	document.getElementById('body').classList.toggle('overflow-hidden');
+	document.getElementById("header").classList.toggle("open");
+	document.getElementById("body").classList.toggle("overflow-hidden");
 });
+
 	var currentScroll = window.pageYOffset || document.body.scrollTop;
 	var lastScrollTop = 0;
-	window.addEventListener('scroll', function(){
+	window.addEventListener("scroll", function(){
 		var currentScroll = window.pageYOffset || document.body.scrollTop;
 		if(currentScroll > lastScrollTop){
-			console.log('down');
+			console.log("down");
 		}else {
-			console.log('up');
+			console.log("up");
 		}
 		lastScrollTop = currentScroll;
 
 	});
+
+	if (lastScrollTop == currentScroll){
+		
+	}
